17799dcc9b9f071ab88982ad74ec64a13a189259
Ramos Felipe <felipeofor@gmail.com>
2021-08-20 16:57:47 -0300

    [clase 4] Promesas

--- Clase_4_Promesas.js
+++ Clase_4_Promesas.js
@@ -1,3 +1,4 @@
+//Lanzar los 5 accesos en forma automática (es decir, el 2do debe lanzarse sin esperar que finalice el 1ero y así sucesivamente)
 function yendoAConsultar1(valor) {
     return new Promise ((resolve, reject)=>{
         setTimeout(()=>{
@@ -53,52 +54,52 @@ function yendoAConsultar5(valor) {
         },4000)
     });
 }
-
+//Luego de lanzar los accesos, pero sin esperar el retorno los mismos, ejecutar un sub-proceso que emita por consola 25 números al azar utilizando funciones generadoras e iteradores
 function *creaIds(){
     let indice = 0;
     while (true) {
         yield Math.random() ;
     }
 }
-
 let creadorIds = creaIds();
 
-
+//Emitir un mensaje previo a comenzar los accesos a internet
 console.log('Iniciando...');
+//Al regresar cada uno de los accesos, emitir el resultado (ok u error) y en caso de retornar ok, imprimir por consola el objeto recibido. 
 yendoAConsultar1('okss')
-    .then(x=>console.log('Volvió la consulta', x))
-    .catch(x=>console.log('Canceló la consulta', x))
-    .finally(()=>console.log('Terminó la consulta'));
-
+    .then(x=>console.log('ok', x))
+    .catch(x=>console.log('error', x))
+    //A su vez, en cada acceso, haya vuelto ok o con error, imprimir por consola un nuevo número al azar utilizando el mismo iterador anterior
+    .finally(()=>console.log(creadorIds.next().value));
+//Al regresar cada uno de los accesos, emitir el resultado (ok u error) y en caso de retornar ok, imprimir por consola el objeto recibido. 
 yendoAConsultar2('okss')
-    .then(x=>console.log('Volvió la consulta', x))
-    .catch(x=>console.log('Canceló la consulta', x))
-    .finally(()=>console.log('Terminó la consulta'));
-
+    .then(x=>console.log('ok', x))
+    .catch(x=>console.log('error', x))
+    //A su vez, en cada acceso, haya vuelto ok o con error, imprimir por consola un nuevo número al azar utilizando el mismo iterador anterior
+    .finally(()=>console.log(creadorIds.next().value));
+//Al regresar cada uno de los accesos, emitir el resultado (ok u error) y en caso de retornar ok, imprimir por consola el objeto recibido. 
 yendoAConsultar3('okss')
-    .then(x=>console.log('Volvió la consulta', x))
-    .catch(x=>console.log('Canceló la consulta', x))
-    .finally(()=>console.log('Terminó la consulta'));
-
+    .then(x=>console.log('ok', x))
+    .catch(x=>console.log('error', x))
+    //A su vez, en cada acceso, haya vuelto ok o con error, imprimir por consola un nuevo número al azar utilizando el mismo iterador anterior
+    .finally(()=>console.log(creadorIds.next().value));
+//Al regresar cada uno de los accesos, emitir el resultado (ok u error) y en caso de retornar ok, imprimir por consola el objeto recibido. 
 yendoAConsultar4('okss')
-    .then(x=>console.log('Volvió la consulta', x))
-    .catch(x=>console.log('Canceló la consulta', x))
-    .finally(()=>console.log('Terminó la consulta'));
-
+    .then(x=>console.log('ok', x))
+    .catch(x=>console.log('error', x))
+    //A su vez, en cada acceso, haya vuelto ok o con error, imprimir por consola un nuevo número al azar utilizando el mismo iterador anterior
+    .finally(()=>console.log(creadorIds.next().value));
+//Al regresar cada uno de los accesos, emitir el resultado (ok u error) y en caso de retornar ok, imprimir por consola el objeto recibido. 
 yendoAConsultar5('okss')
-    .then(x=>console.log('Volvió la consulta', x))
-    .catch(x=>console.log('Canceló la consulta', x))
-    .finally(()=>console.log('Terminó la consulta'));
+    .then(x=>console.log('ok', x))
+    .catch(x=>console.log('error', x))
+    //A su vez, en cada acceso, haya vuelto ok o con error, imprimir por consola un nuevo número al azar utilizando el mismo iterador anterior
+    .finally(()=>console.log(creadorIds.next().value));
 console.log('Finalizando...');
 
+//Luego de lanzar los accesos, pero sin esperar el retorno los mismos, ejecutar un sub-proceso que emita por consola 25 números al azar utilizando funciones generadoras e iteradores
 function creador() {for (i=0; i<25; i++){
     console.log(creadorIds.next().value);
 }}
 
 creador();
-
-
-// console.log(creadorIds.next().value);
-// console.log(creadorIds.next().value);
-// console.log(creadorIds.next().value);
-// console.log(creadorIds.next().value);
